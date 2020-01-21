8ff10376f4e614db9c9c6cc776bef4341cc46c8f
vipulchakravarthy <saivipul15@gmai.com>
2020-01-21 11:09:52 +0530

    Life cycle methods

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,27 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import Click from './components/Click';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import NameList from './components/NameList';
-import Stylesheet from "./components/Stylesheet";
-import InLine from './components/InLine';
-import './appStyles.css';
-import styles from './appStyles.module.css';
 
-import Form from './components/Form';
 
 class App extends Component {
   render(){
     return (
     <div className="App">
-      <Form />
     </div>
     );
   }
---
+++ react-tutorial/src/components/LifecycleA.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class LifecycleA extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default LifecycleA
