3577ece35998871fe3a0585b7c04b9de221a1645
vipulchakravarthy <saivipul15@gmai.com>
2020-01-24 12:30:00 +0530

    Vidly Project

---
+++ vidly/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Movies from './components/movies';
+
+function App() {
+
+  return (
+    
+  <main className="container">
+    <Movies/>
+  </main>
+  );
+}
+
+export default App;
