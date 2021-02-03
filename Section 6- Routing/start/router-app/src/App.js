ff98594ce065a788ed9e091f8131f7433183c275
vipulchakravarthy <saivipul15@msitprogram.net>
2021-02-03 11:23:37 +0530

    added switch for routing

--- Section 6- Routing/start/router-app/src/App.js
+++ Section 6- Routing/start/router-app/src/App.js
@@ -6,6 +6,9 @@ import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
 import ProductDetails from "./components/productDetails";
 import NotFound from "./components/notFound";
+
+import {Route, Switch} from 'react-router-dom';
+
 import "./App.css";
 
 class App extends Component {
@@ -13,6 +16,14 @@ class App extends Component {
     return (
       <div>
         <NavBar />
+        <div className="content">
+          <Switch>
+          <Route path="/products" component={Products}/>
+          <Route path="/posts" component={Posts}/>
+          <Route path="/admin" component={Dashboard}/>
+          <Route path="/" component={Home}/>
+          </Switch>
+        </div>
       </div>
     );
   }
