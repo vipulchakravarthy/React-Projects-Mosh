e166d79a02e65d7b295f081a1d249319c4f598bf
vipulchakravarthy <saivipul15@msitprogram.net>
2021-02-09 10:16:44 +0530

    added handle submit method

--- Section 6- Routing/start/vidly/src/App.js
+++ Section 6- Routing/start/vidly/src/App.js
@@ -8,6 +8,7 @@ import MovieForm from './components/movieForm';
 
 import {Route, Redirect, Switch} from 'react-router-dom';
 import NavBar from './components/navbar';
+import LoginForm from './components/loginForm';
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
       <NavBar />
       <main className="container">
         <Switch>
+        <Route path="/login" component={LoginForm}></Route>
         <Route path="/movies/:id" component={MovieForm}></Route>
         <Route path="/movies" component={Movies}></Route>
         <Route path="/customers" component={Customers}></Route>
--- Section 6- Routing/start/vidly/src/components/loginForm.jsx
+++ Section 6- Routing/start/vidly/src/components/loginForm.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+
+    state = {
+        account: {
+            username: '',
+            password: ''
+        }
+    }
+    
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log("submitted")
+    }
     render() { 
         return ( <div>
             <h1>Login</h1>
-            <form >
+            <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
-                <input id='username' type="text" className="form-control"/>
+                <input autoFocus id='username' type="text" className="form-control"/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
