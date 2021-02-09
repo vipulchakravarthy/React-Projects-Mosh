924b661d71884c434a836f00a5df759c5c1cf4b6
vipulchakravarthy <saivipul15@msitprogram.net>
2021-02-09 11:35:55 +0530

    added handle change method

--- Section 6- Routing/start/vidly/src/components/loginForm.jsx
+++ Section 6- Routing/start/vidly/src/components/loginForm.jsx
@@ -14,17 +14,30 @@ class LoginForm extends Component {
 
         console.log("submitted")
     }
-    render() { 
+
+    handleChange = ({currentTarget: input}) => {
+        console.log("called change")
+        const account = {...this.state.account}
+        account[input.name] = input.value;
+        this.setState({account})
+    }
+    render() {
+        const {account} = this.state 
         return ( <div>
             <h1>Login</h1>
             <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
-                <input autoFocus id='username' type="text" className="form-control"/>
+                <input 
+                value={account.username} 
+                onChange={this.handleChange}
+                autoFocus id='username' name="username" type="text" className="form-control"/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input id='password' type="text" className="form-control"/>
+                    <input 
+                    onChange={this.handleChange}
+                    value={account.password} name='password' id='password' type="text" className="form-control"/>
                 </div>
                 <button className="btn-primary">Login</button>
             </form>
